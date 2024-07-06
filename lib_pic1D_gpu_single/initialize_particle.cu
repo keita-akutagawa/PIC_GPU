f65bed3d81b09a1738d4b70d11f12351fb94cdb6
Keita <keita19990517@gmail.com>
2024-07-07 00:37:15 +0900

    20240706

--- lib_pic1D_gpu_single/initialize_particle.cu
+++ lib_pic1D_gpu_single/initialize_particle.cu
@@ -49,12 +49,8 @@ __global__ void maxwellDistributionForVelocity_kernel(
     unsigned long long i = blockIdx.x * blockDim.x + threadIdx.x;
 
     if (i < nEnd - nStart) {
-        thrust::default_random_engine rng1(seed);
-        thrust::default_random_engine rng2(seed + 100);
-        thrust::default_random_engine rng3(seed + 200);
-        rng1.discard(3 * i);
-        rng2.discard(3 * i + 1);
-        rng3.discard(3 * i + 2);
+        thrust::default_random_engine rng(seed + i);
+        rng.discard(i);
         thrust::random::normal_distribution<float> dist_vx(bulkVxSpecies, vThSpecies);
         thrust::random::normal_distribution<float> dist_vy(bulkVySpecies, vThSpecies);
         thrust::random::normal_distribution<float> dist_vz(bulkVzSpecies, vThSpecies);
@@ -62,9 +58,9 @@ __global__ void maxwellDistributionForVelocity_kernel(
         float vx, vy, vz;
 
         while (true) {
-            vx = dist_vx(rng1);
-            vy = dist_vy(rng2);
-            vz = dist_vz(rng3);
+            vx = dist_vx(rng);
+            vy = dist_vy(rng);
+            vz = dist_vz(rng);
 
             if (vx * vx + vy * vy + vz * vz < device_c * device_c) break;
         }
