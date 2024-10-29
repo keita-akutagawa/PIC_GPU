a7d12b6aa2d5572700ed7efe8e65dbff58ad3922
Keita-A <keita19990517@gmail.com>
2024-10-29 19:50:42 +0900

    20241029

---
+++ PIC_multiGPU/examples2D/two_stream/main_two_stream.cu
@@ -0,0 +1,180 @@
+#include "main_two_stream_const.hpp"
+
+
+__global__ void initializeField_kernel(
+    ElectricField* E, MagneticField* B, MPIInfo* device_mPIInfo
+)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (i < device_nx && j < device_ny) {
+        MPIInfo mPIInfo = *device_mPIInfo;
+
+        if (mPIInfo.isInside(i, j)) {
+            int index = mPIInfo.globalToLocal(i, j);
+            E[index].eX = 0.0;
+            E[index].eY = 0.0;
+            E[index].eZ = 0.0;
+            B[index].bX = device_B0;
+            B[index].bY = 0.0;
+            B[index].bZ = 0.0;
+        }
+    }
+}
+
+void PIC2D::initialize()
+{
+    float xminForProcs = xmin + (xmax - xmin) / mPIInfo.gridX * mPIInfo.localGridX;
+    float xmaxForProcs = xmin + (xmax - xmin) / mPIInfo.gridX * (mPIInfo.localGridX + 1);
+    float yminForProcs = ymin + (ymax - ymin) / mPIInfo.gridY * mPIInfo.localGridY;
+    float ymaxForProcs = ymin + (ymax - ymin) / mPIInfo.gridY * (mPIInfo.localGridY + 1);
+
+    initializeParticle.uniformForPositionX(
+        0, mPIInfo.existNumIonPerProcs, 
+        xminForProcs, xmaxForProcs, 
+        0, particlesIon
+    );
+    initializeParticle.uniformForPositionX(
+        0, mPIInfo.existNumElectronPerProcs, 
+        xminForProcs, xmaxForProcs, 
+        100, particlesElectron
+    );
+    initializeParticle.uniformForPositionY(
+        0, mPIInfo.existNumIonPerProcs, 
+        yminForProcs, ymaxForProcs, 
+        200, particlesIon
+    );
+    initializeParticle.uniformForPositionY(
+        0, mPIInfo.existNumElectronPerProcs, 
+        yminForProcs, ymaxForProcs, 
+        300, particlesElectron
+    );
+
+    initializeParticle.maxwellDistributionForVelocity(
+        0.0f, 0.0f, 0.0f, 
+        vThIon, vThIon, vThIon, 
+        0, mPIInfo.existNumIonPerProcs, 400, particlesIon
+    );
+    initializeParticle.maxwellDistributionForVelocity(
+        -10.0f * vThIon, 0.0f, 0.0f, 
+        vThElectron, vThElectron, vThElectron, 
+        0, mPIInfo.existNumElectronPerProcs / 2, 500, particlesElectron
+    );
+    initializeParticle.maxwellDistributionForVelocity(
+        +10.0f * vThIon, 0.0f, 0.0f,  
+        vThElectron, vThElectron, vThElectron, 
+        mPIInfo.existNumElectronPerProcs / 2, mPIInfo.existNumElectronPerProcs, 600, particlesElectron
+    );
+
+    dim3 threadsPerBlock(16, 16);
+    dim3 blocksPerGrid((nx + threadsPerBlock.x - 1) / threadsPerBlock.x,
+                       (ny + threadsPerBlock.y - 1) / threadsPerBlock.y);
+
+    initializeField_kernel<<<blocksPerGrid, threadsPerBlock>>>(
+        thrust::raw_pointer_cast(E.data()), thrust::raw_pointer_cast(B.data()), 
+        device_mPIInfo
+    );
+    cudaDeviceSynchronize();
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    sendrecv_field(B, mPIInfo);
+    sendrecv_field(E, mPIInfo);
+    sendrecv_field(current, mPIInfo);
+
+    boundary.periodicBoundaryBX(B);
+    boundary.periodicBoundaryBY(B);
+    boundary.periodicBoundaryEX(E);
+    boundary.periodicBoundaryEY(E);
+    boundary.periodicBoundaryCurrentX(current);
+    boundary.periodicBoundaryCurrentY(current);
+    boundary.boundaryForInitialize(particlesIon, mPIInfo.existNumIonPerProcs);
+    boundary.boundaryForInitialize(particlesElectron, mPIInfo.existNumElectronPerProcs);
+    
+    MPI_Barrier(MPI_COMM_WORLD);
+}
+
+
+int main(int argc, char** argv)
+{
+    MPI_Init(&argc, &argv);
+
+    MPIInfo mPIInfo;
+    setupInfo(mPIInfo, buffer);
+
+    if (mPIInfo.rank == 0) {
+        std::cout << mPIInfo.gridX << "," << mPIInfo.gridY << std::endl;
+        mpifile   << mPIInfo.gridX << "," << mPIInfo.gridY << std::endl;
+    }
+
+    cudaSetDevice(mPIInfo.rank);
+
+    initializeDeviceConstants();
+
+    mPIInfo.existNumIonPerProcs = int(totalNumIon / mPIInfo.procs);
+    mPIInfo.existNumElectronPerProcs = int(totalNumElectron / mPIInfo.procs);
+    mPIInfo.totalNumIonPerProcs = mPIInfo.existNumIonPerProcs
+                                + numberDensityIon * (mPIInfo.localSizeX + mPIInfo.localSizeY) * (2 * mPIInfo.buffer + 10);
+    mPIInfo.totalNumElectronPerProcs = mPIInfo.existNumElectronPerProcs
+                                     + numberDensityElectron * (mPIInfo.localSizeX + mPIInfo.localSizeY) * (2 * mPIInfo.buffer + 10);
+
+    PIC2D pIC2D(mPIInfo);
+
+    pIC2D.initialize();
+
+    if (mPIInfo.rank == 0) {
+        size_t free_mem = 0;
+        size_t total_mem = 0;
+        cudaError_t status = cudaMemGetInfo(&free_mem, &total_mem);
+
+        std::cout << "Free memory: " << free_mem / (1024 * 1024) << " MB" << std::endl;
+        std::cout << "Total memory: " << total_mem / (1024 * 1024) << " MB" << std::endl;
+
+        std::cout << "exist number of partices is " 
+                  << mPIInfo.procs * (mPIInfo.existNumIonPerProcs + mPIInfo.existNumElectronPerProcs) 
+                  << std::endl;
+        std::cout << "exist number of partices + buffer particles is " 
+                  << mPIInfo.procs * (mPIInfo.totalNumIonPerProcs + mPIInfo.totalNumElectronPerProcs) 
+                  << std::endl;
+        std::cout << std::setprecision(4) 
+                << "omega_pe * t = " << totalStep * dt * omegaPe << std::endl;
+    }
+
+
+    for (int step = 0; step < totalStep + 1; step++) {
+        MPI_Barrier(MPI_COMM_WORLD);
+
+        if (step % fieldRecordStep == 0) {
+            if (mPIInfo.rank == 0) {
+                std::cout << std::to_string(step) << " step done : total time is "
+                            << std::setprecision(6) << totalTime
+                            << std::endl;
+                logfile << std::to_string(step) << "," << totalTime << std::endl;
+            }
+            pIC2D.saveFields(
+                directoryname, filenameWithoutStep, step
+            );
+            pIC2D.saveParticle(
+                directoryname, filenameWithoutStep, step
+            );
+        }
+        
+        pIC2D.oneStepPeriodicXY();
+
+        if (mPIInfo.rank == 0) {
+            totalTime += dt;
+        }
+    }
+
+    MPI_Finalize();
+
+    if (mPIInfo.rank == 0) {
+        std::cout << "program was completed!" << std::endl;
+    }
+
+    return 0;
+}
+
+
+
---
+++ PIC_multiGPU/examples2D/two_stream/main_two_stream_const.hpp
@@ -0,0 +1,140 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include "../../PIC2D_multiGPU/pic2D.hpp"
+#include "../../PIC2D_multiGPU/mpi.hpp"
+#include <cuda_runtime.h>
+
+
+std::string directoryname = "/cfca-work/akutagawakt/PIC_multiGPU/results_two_stream";
+std::string filenameWithoutStep = "two_stream";
+std::ofstream logfile("/cfca-work/akutagawakt/PIC_multiGPU/results_two_stream/log_two_stream.txt");
+std::ofstream mpifile("/cfca-work/akutagawakt/PIC_multiGPU/results_two_stream/mpi_two_stream.txt");
+
+const int buffer = 2;
+
+const int totalStep = 10000;
+const int fieldRecordStep = 100;
+const bool isParticleRecord = true;
+const int particleRecordStep = 1000000;
+float totalTime = 0.0f;
+
+const float c = 1.0f;
+const float epsilon0 = 1.0f;
+const float mu0 = 1.0f;
+const float dOfLangdonMarderTypeCorrection = 0.0f;
+const float EPS = 1.0e-10f;
+
+const int numberDensityIon = 100;
+const int numberDensityElectron = 100;
+
+const float B0 = sqrt(static_cast<double>(numberDensityElectron)) / 10.0;
+
+const float mRatio = 100.0f;
+const float mElectron = 1.0f;
+const float mIon = mRatio * mElectron;
+
+const float tRatio = 100.0f;
+const float tElectron = 0.5f * mElectron * pow(0.01f * c, 2);
+const float tIon = tRatio * tElectron;
+
+const float qRatio = -1.0f;
+const float qElectron = -1.0f * sqrt(epsilon0 * tElectron / static_cast<float>(numberDensityElectron)) / 1.0f;
+const float qIon = qRatio * qElectron;
+
+const float omegaPe = sqrt(static_cast<float>(numberDensityElectron) * pow(qElectron, 2) / mElectron / epsilon0);
+const float omegaPi = sqrt(static_cast<float>(numberDensityIon) * pow(qIon, 2) / mIon / epsilon0);
+const float omegaCe = abs(qElectron * B0 / mElectron);
+const float omegaCi = qIon * B0 / mIon;
+
+const float debyeLength = sqrt(epsilon0 * tElectron / static_cast<float>(numberDensityElectron) / pow(qElectron, 2));
+const float ionInertialLength = c / omegaPi;
+
+const int nx = 512;
+const float dx = 1.0f;
+const float xmin = EPS; 
+const float xmax = nx * dx - EPS;
+
+const int ny = 4;
+const float dy = 1.0f;
+const float ymin = EPS; 
+const float ymax = ny * dy - EPS;
+
+float dt = 0.5f;
+
+const unsigned long long totalNumIon = numberDensityIon * nx * ny;
+const unsigned long long totalNumElectron = numberDensityElectron * nx * ny;
+const unsigned long long totalNumParticles = totalNumIon + totalNumElectron;
+
+const float vThIon = sqrt(2.0f * tIon / mIon);
+const float vThElectron = sqrt(2.0f * tElectron / mElectron);
+const float bulkVxIon = 0.0f;
+const float bulkVyIon = 0.0f;
+const float bulkVzIon = 0.0f;
+const float bulkVxElectron = 0.0f;
+const float bulkVyElectron = 0.0f;
+const float bulkVzElectron = 0.0f;
+
+
+__constant__ int device_totalStep;
+__device__ float device_totalTime;
+
+__constant__ float device_c;
+__constant__ float device_epsilon0;
+__constant__ float device_mu0;
+__constant__ float device_dOfLangdonMarderTypeCorrection;
+__constant__ float device_EPS;
+
+__constant__ int device_numberDensityIon;
+__constant__ int device_numberDensityElectron;
+
+__constant__ float device_B0;
+
+__constant__ float device_mRatio;
+__constant__ float device_mElectron;
+__constant__ float device_mIon;
+
+__constant__ float device_tRatio;
+__constant__ float device_tElectron;
+__constant__ float device_tIon;
+
+__constant__ float device_qRatio;
+__constant__ float device_qElectron;
+__constant__ float device_qIon;
+
+__constant__ float device_omegaPe;
+__constant__ float device_omegaPi;
+__constant__ float device_omegaCe;
+__constant__ float device_omegaCi;
+
+__constant__ float device_debyeLength;
+__constant__ float device_ionInertialLength;
+
+__constant__ int device_nx;
+__constant__ float device_dx;
+__constant__ float device_xmin;
+__constant__ float device_xmax;
+
+__constant__ int device_ny;
+__constant__ float device_dy;
+__constant__ float device_ymin;
+__constant__ float device_ymax;
+
+__device__ float device_dt;
+
+__constant__ unsigned long long device_totalNumIon;
+__constant__ unsigned long long device_totalNumElectron;
+__constant__ unsigned long long device_totalNumParticles;
+
+__constant__ float device_vThIon;
+__constant__ float device_vThElectron;
+__constant__ float device_bulkVxIon;
+__constant__ float device_bulkVyIon;
+__constant__ float device_bulkVzIon;
+__constant__ float device_bulkVxElectron;
+__constant__ float device_bulkVyElectron;
+__constant__ float device_bulkVzElectron;
+
+
